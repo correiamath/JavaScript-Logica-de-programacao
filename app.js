c442ade1507a965cc32566dbc24e941bd596b2d9
math <matheus.b.correia7@gmail.com>
2024-02-20 00:49:55 -0300

    Update files

--- app.js
+++ app.js
@@ -10,8 +10,27 @@ alterarCampo("p", "Escolha um numero entre 1 e 10");
 
 function verificarChute() {
   console.log(numeroAleatorio);
+  alert(comparar());
 }
 
 function gerarNumeroAleatorio() {
   return parseInt(Math.random() * 10 + 1);
+}
+
+function comparar() {
+  let input = parseInt(document.querySelector("input").value);
+  let resultado;
+
+  if (input > numeroAleatorio) {
+    resultado = "O valor inserido é maior que o número secreto";
+    alert(resultado);
+  } else if (input < numeroAleatorio) {
+    resultado = "O valor inserido é menor que o número secreto";
+    alert(resultado);
+  } else {
+    resultado = "Você acertou!!!";
+    alert(resultado);
+  }
+
+  return resultado;
 }
